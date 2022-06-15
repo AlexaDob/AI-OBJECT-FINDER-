1d0b244ed3b560cb89bc5efe3b6cad01b1732870
AlexaDob <58271470+AlexaDob@users.noreply.github.com>
2022-06-15 11:53:03 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
 status2 = "";
+objects = []
 
 function setup() {
   canvas = createCanvas(480, 380);
@@ -22,13 +23,42 @@ function modelLoaded() {
 
 function gotResult(error, results) {
   if (error) {
-    console.log(error);
+    console.log(error);obj
   }
   console.log(results);
   objects = results;
 }
 
+function draw(){
+    image(video, 0, 0, 480, 380)
+    
+    if (status2 != ""){
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status : Object Detected";
 
-function draw() {
-  image(video, 0, 0, 480, 380);
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+  
+
+            if(objects[i].label == object_name)
+          {
+            video.stop();
+            objectDetector.detect(gotResult);
+            document.getElementById("object_status").innerHTML = object_name + " Found";
+            synth = window.speechSynthesis;
+            utterThis = new SpeechSynthesisUtterance(object_name + "Found");
+            synth.speak(utterThis);
+          }
+          else
+          {
+            document.getElementById("object_status").innerHTML = object_name + " Not Found";
+          }          
+        }
+
+    }
 }
